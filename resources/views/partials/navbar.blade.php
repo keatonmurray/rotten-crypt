9ce70c2766d19ff4724c87202181b60d0d6ab0ea
Kate <murraykeaton422@gmail.com>
2023-12-11 02:24:29 -0500

    adjusted bg image position. it is now responsive

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg d-lg-flex d-block py-4" style="background: #000;">
+    <div class="container">
+      <a href="/" class="navbar-brand text-white fs-2 fw-bold text-uppercase" id="danville" href="#">Rotten Crypt</a href="/">
+      <button class="navbar-toggler" style="padding-top: -5px" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+        <span><i class="fa-solid fa-bars" style="color: #ffffff;"></i></span>
+      </button>
+      <style>
+        #navbarsExample07.in, #navbarsExample07.collapsing {
+          display: inline-block;
+          width: 100%;
+          min-width: 40%;
+        }
+      </style>
+      <div class="collapse navbar-collapse" id="navbarsExample07">
+        <ul class="navbar-nav mt-1">
+          <li class="nav-item">
+            <a class="nav-link active" href="" id="danville">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href=""  id="danville">News</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="" id="danville">Discographies</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="" id="danville">Podcasts</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="" id="danville">Store</a>
+          </li>
+        </ul>
+        <a class="btn text-uppercase" id="danville" href="">Join at your own risk</a>
+      </div>
+    </div>
+</nav>
