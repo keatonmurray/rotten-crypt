8c0f892d8e6099469d21b4b073377c223afeb7e8
Kate <murraykeaton422@gmail.com>
2023-12-14 20:19:38 -0500

    modified blade templates

--- resources/views/music/index.blade.php
+++ resources/views/music/index.blade.php
@@ -1,1 +1,6 @@
-MUSIC
+@extends('main.layout')
+@section('content')
+    <div class="container">
+        <h4 class="text-white">Music page</h4>
+    </div>
+@endsection
--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -1,6 +1,6 @@
 @extends('main.layout')
 @section('content')
     <div class="container">
-        
+        <h4 class="text-white">News page</h4>
     </div>
 @endsection
