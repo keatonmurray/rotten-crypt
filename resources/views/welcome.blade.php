84ae85e1a4c68ed8840a0c5c8e112d0ca7b5fab3
Kate <murraykeaton422@gmail.com>
2023-12-14 20:49:58 -0500

    declared route for the browse music button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
                     <p class="lead fs-5 text-white" id="danville">Death Metal release updates, news, music videos, live shows, podcasts, and more. </p>
                     <br>
                     <div class="d-grid justify-content-md-start">
-                        <a href="" type="button" class="btn btn-light btn-lg px-4 me-md-2">
+                        <a href="/discographies" type="button" class="btn btn-light btn-lg px-4 me-md-2">
                         <span id="danville">
                             <i class="fa-solid fa-circle-play"></i>  Browse Music
                         </span>
